342bf578bc1799903bff0037d1360e69ea42b5ff
kamila2508 <kamila3602@gmail.com>
2023-07-19 11:09:14 +0200

    Add category model

--- shopping_list_app/models.py
+++ shopping_list_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User, AbstractUser
+from django.db.models import SET_NULL
 
 UNITS = (
     ('g', 'gram'),
@@ -13,10 +14,18 @@ UNITS = (
 )
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.name
+
+
 class Ingredient(models.Model):
     name = models.CharField(max_length=64)
     quantity = models.FloatField()
     unit = models.CharField(choices=UNITS, default='g', max_length=32)
+    category = models.ForeignKey(Category, blank=True, null=True, on_delete=SET_NULL)
 
     def __str__(self):
         return self.name
